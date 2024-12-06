fe9d6494e665d991aa415eeccf0f61719b835b41
Rafael Miralha <rafamiralha1@gmail.com>
2024-12-06 16:01:49 -0300

    feat: adicionando uma interface de login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { LoginComponent } from '../login/login.component';
+import {  CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LoginComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent  {
+public show_login = false
+public show_content = true
+openModalLogin(){
+  this.show_login = true;
+  this.show_content = false;
+}
+
 
 }
