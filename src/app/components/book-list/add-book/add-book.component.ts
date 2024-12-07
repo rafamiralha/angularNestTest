00f8d5154b1deff698b29516664c4031e4e9d253
Rafael Miralha <rafamiralha1@gmail.com>
2024-12-07 13:36:53 -0300

    fix: organização das pastas do projeto acoplando componente Filho a componente Pai.

--- src/app/components/book-list/add-book/add-book.component.ts
+++ src/app/components/book-list/add-book/add-book.component.ts
@@ -5,7 +5,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { BookService } from '../../service/book.service';
+import { BookService } from '../../../service/book.service';
 import { Router } from '@angular/router';
 
 @Component({
